39b39de3a9e7543b3cc6457168bd85b2c2cb27a3
sameera-87 <sameekumarasinghe87@gmail.com>
2025-05-21 14:50:14 +0530

    Lab 02 completed

--- CO321_Lab02/task4.c
+++ CO321_Lab02/task4.c
@@ -6,14 +6,14 @@ volatile uint8_t value = 0;
 
 
 ISR(INT0_vect) {
-    _delay_ms(100);
+    _delay_ms(500);
     value = (value << 1); 
     PORTB = value & 0x3F; 
 }
 
 
 ISR(INT1_vect) {
-    _delay_ms(100);
+    _delay_ms(500);
     value = (value << 1) | 0x01; 
     PORTB = value & 0x3F;        
 }
@@ -22,15 +22,20 @@ int main(void) {
     
     DDRB = 0x3F; 
 
-    
     DDRD &= ~((1 << PD2) | (1 << PD3));
 
     PORTD |= (1 << PD2) | (1 << PD3);
 
     EIMSK |= (1 << INT0) | (1 << INT1);
 
-    EICRA |= (1 << ISC01) | (1 << ISC11);  
-    EICRA &= ~((1 << ISC00) | (1 << ISC10)); 
+    EICRA |= (1 << ISC01);   // Set ISC01 = 1
+    EICRA &= ~(1 << ISC00);  // Set ISC00 = 0
+
+    EICRA |= (1 << ISC11);   
+    EICRA &= ~(1 << ISC10);
+
+    // EICRA |= (1 << ISC01) | (1 << ISC11);  
+    // EICRA &= ~((1 << ISC00) | (1 << ISC10)); 
 
     sei();
 
